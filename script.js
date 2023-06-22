7cd2e3d1c7e93ef1440076e01554b1bff3d8783b
meganklu <88399770+meganklu@users.noreply.github.com>
2023-06-22 03:39:49 +0000

    scroll down button

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-$(document).ready(function () {
-    // Handler for .ready() called.
-    $('html, body').animate({
-        scrollTop: $('#intro').offset().top
-    }, 'slow');
-});
+// Scroll to starting hold when page loads
+window.onload = function () {
+  var scrollDiv = document.getElementById("intro").offsetTop;
+	window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
 
+// Mobile hover
 document.addEventListener("touchstart", function() {}, true);
+
+// Get the button:
+let downButton = document.getElementById("backDown");
+
+// If element out of viewport, show button
+window.onscroll = function() {
+  var introHold = document.getElementById("intro");
+  var bounding = introHold.getBoundingClientRect();
+
+  if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+    downButton.style.display = "none";
+  } else {
+    downButton.style.display = "block";
+  }
+};
+
+// When the user clicks on the button, scroll to the bottom of the document
+function scrollDown() {
+	var scrollDiv = document.getElementById("intro").offsetTop;
+	window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
